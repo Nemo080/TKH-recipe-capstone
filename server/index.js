4860644220b7ffd59d52b32bb3d15156faa07746
DannyCastle1 <castillodaniel1@yahoo.com>
2024-05-01 18:56:34 -0400

    added a route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from 'cors';
 
 const app = express();
+const router = express.Router();
 
+app.use(express.json());
+app.use(cors());
+
+app.use('/test', (req, res)=>{
+    res.send('Route works');
+});
 app.listen(3000, ()=>{
     console.log('Server is running')
 });
 
+export default router;
