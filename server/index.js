6c7396dcf0020892d9d4522a2dc4e4824dad2800
DannyCastle1 <castillodaniel1@yahoo.com>
2024-05-01 18:20:48 -0400

    demo feature

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.send('working test route');
+})
 app.listen(3000, ()=>{
     console.log('Server is running')
 });
