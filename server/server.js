f3064fe33c618c706544d041356c9e0557a5b774
Nemo080 <nishatf01@gmail.com>
2024-05-06 15:41:46 -0400

    server set up

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const logger = (req, _res, next) => {
+    const time = new Date().toLocaleTimeString();
+    console.log(`${time} ${req.method}: ${req.url}`);
+    next();
+};
+  
+app.use(logger);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on <http://localhost>:${PORT}`);
+});
