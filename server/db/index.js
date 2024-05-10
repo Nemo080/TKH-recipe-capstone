3d95fa07565b1d06754596a0eecd457872be0c5f
Nemo080 <nishatf01@gmail.com>
2024-05-10 15:38:11 -0400

    auth fixes

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 //Instantiate a new prisma client instead of constantly creating new clients per route.
 //The prisma client knows when to open and close connections to the db
 const prisma = new PrismaClient();
 
-module.exports = prisma;
+export default prisma;
