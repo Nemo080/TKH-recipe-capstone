ba0de6e88a30438b37265219724ed1397a31fb2d
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2024-05-27 20:09:33 -0400

    Updated frontend router for CRUD Recipe

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home.jsx'
+import Login from './components/Login.jsx'
+import Recipes from './components/Recipes.jsx'
+
+
+import Signup from './components/Signup';
+
+import CRUDRecipe from './components/CRUDRecipe';
+
+
+function App() {
+
+  return (
+    //frontend routing
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/recipes' element={<Recipes/>}/>
+      <Route path="/crud-recipe" element={<CRUDRecipe/>}/>
+    </Routes>
+  )
+}
+
+export default App
