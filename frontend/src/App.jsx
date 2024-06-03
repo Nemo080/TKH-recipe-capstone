8733552cbdb8af97ade9393d23e9b1f803de5824
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2024-06-02 23:17:43 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Recipes from './components/Recipes.jsx'
 
 import Signup from './components/Signup';
 
-import CreateRecipe from './components/CreateRecipe';
+import CRUDRecipe from './components/CRUDRecipe';
 
 
 
@@ -20,7 +20,7 @@ function App() {
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/recipes' element={<Recipes/>}/>
-      <Route path="/create-recipe" element={<CreateRecipe/>}/>
+      <Route path="/crud-recipe" element={<CRUDRecipe/>}/>
       <Route path='/cuisine/:type' element={<Recipes/>}/>
     </Routes>
   )
