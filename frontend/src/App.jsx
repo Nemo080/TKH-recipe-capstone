7f10caf187e189d56a59da8d486f703e592825b6
DannyCastle1 <castillodaniel1@yahoo.com>
2024-06-10 01:20:54 -0400

    working on the dropdown filtering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,12 @@ import './index.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home.jsx'
 import Login from './components/Login.jsx'
-<<<<<<< Updated upstream
 import Recipes from './components/Recipes.jsx';
 import Navbar from './components/Navbar';
 import Signup from './components/Signup';
 
 import CRUDRecipe from './components/CRUDRecipe';
 
-=======
-import Signup from './components/Signup';
-import RecipeForm from './components/CRUDRecipe';
-import Recipes from './components/Recipes.jsx'
->>>>>>> Stashed changes
 
 
 
@@ -22,7 +16,6 @@ function App() {
 
   return (
     //frontend routing
-<<<<<<< Updated upstream
     <BrowserRouter>
       <Navbar />
         <Routes>
@@ -33,15 +26,6 @@ function App() {
           <Route path="/crud-recipe" element={<CRUDRecipe/>}/>
         </Routes>
     </BrowserRouter>
-=======
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/recipes' element={<Recipes/>}/>
-      <Route path="/crud-recipe" element={<RecipeForm/>}/>
-    </Routes>
->>>>>>> Stashed changes
   )
 }
 
--- frontend/src/components/Recipes.jsx
+++ frontend/src/components/Recipes.jsx
@@ -1,93 +1,98 @@
 import { useEffect, useState} from "react";
+import { Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 
 // function that makes an API call to get a random set of recipes
 function Recipes(){
  const [recipe, setRecipe] = useState([]);
- const [localRecipe, setLocalRecipe] = useState([]);
  const [filteredRecipe, setFilteredRecipe] = useState([]);
- const [cuisine, setCuisine] = useState([]);
+ const [mealType, setMealType] = useState([]);
 
  useEffect(()=>{
-    getRecipes();
-    localRecipes();
-    },[filteredRecipe]);
+    Promise.all(
+        [ getRecipes(), localRecipes()]
+    ).then(
+        (values) =>{
+            console.log(values);
+            setFilteredRecipe([...values[0], ...values[1]]);
+            setRecipe([...values[0], ...values[1]]);
+        }
+    )
+    // getRecipes();
+    // localRecipes();
+    },[]);
 // function that retrieves recipes saved in the database
     const localRecipes = async () =>{
         const data = await fetch('http://localhost:3000/recipe/recipes');
-        const recipes = await data.json();
-        console.log(recipes)
-        setLocalRecipe(data.recipes);
+        const fetchedData = await data.json();
+        console.log(fetchedData)
+       return fetchedData.recipes
     };
 // function that retrieves recipes from spoonacular
     const getRecipes = async () =>{
        const check = localStorage.getItem('recipes');
        if (check) {
-        setRecipe(JSON.parse(check));
+        return JSON.parse(check);
        } else {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_API_KEY}&includeNutrition=true&number=5`)
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_API_KEY}&includeNutrition=true&number=6`)
         const data = await api.json();
         localStorage.setItem('recipes', JSON.stringify(data.recipes));
-        setRecipe(data.recipes);
+        // setRecipe(data.recipes);
         console.log(data.recipes);
+        return data.recipes
+       
        }
     };
-    const handleCuisineChange = (e) =>{
-        setCuisine(e.target.value);
+    const handleMealTypeChange = (e) =>{
+        setMealType(e.target.value);
         filteredRecipes();
     };
     const filteredRecipes = () => {
-        const allRecipes = [...recipe, ...localRecipe];
+        const allRecipes = [...recipe];
         const filteredRecipe = allRecipes.filter((recipe)=>{
-            if (cuisine === 'italian'){
-                return recipe.cuisine === 'italian';
-            } else if (cuisine === 'american'){
-                return recipe.cuisine === 'american';
-            } else {
-                return filteredRecipe;
-            }
+            return recipe.dishTypes.includes(mealType)
         });
+        console.log(filteredRecipe);
         setFilteredRecipe(filteredRecipe);
-        return (
-            <div>
-                <select value={cuisine} onChange={handleCuisineChange}>
-                    <option value=''>Select a cuisine</option>
-                    <option value='italian'>Italian</option>
-                    <option value='american'>American</option>
-                </select>
-                <ul>
-                    {filteredRecipes.map((recipe)=>{
-                        <li key={recipe.id}>{recipe.name}</li>
-                    })}
-                </ul>
-            </div>
-        )
+
     }
+    console.log(filteredRecipe)
     return(
         <>
         <div>
-        {localRecipe && localRecipe.map(item=>{
-                return(
-                    <div key={item.id}>
-                        <p>{item.title}</p>
-                        <p>{item.ingredients}</p>
-                        <p>{item.equipment}</p>
-                        <p>{item.directions}</p>
-                        <p>{item.author}</p>
-                    </div>
-                )
-            })}
+        <select value={mealType} onChange={handleMealTypeChange}>
+                    <option value=''>Select a type of meal</option>
+                    <option value='breakfast'>Breakfast</option>
+                    <option value='dinner'>Dinner</option>
+                </select>
+                <ul>
+                    {filteredRecipe.map((recipe)=>{
+                        <div key={recipe.id}>
+                        <p>{recipe.title}</p>
+                        <p>{recipe.ingredients}</p>
+                        <p>{recipe.equipment}</p>
+                        <p>{recipe.directions}</p>
+                        <p>{recipe.author}</p>
+                        </div>
+                    })}
+                </ul>
         </div>
         <div className="recipe-card">
+            <Splide options={{perPage: 3, gap: '4rem', pagination: false,}}>
             {recipe.map(item=>{
                 return(
-                    <div key={item.id}>
+                    <SplideSlide key={item.id}>
+                    <div>
                         <p>{item.title}</p>
                         <img className="recipe-image" 
                         src={item.image} alt={item.title}/>
                     </div>
+                    </SplideSlide>
                 );
             })};
+            </Splide>
+
         </div>
         </>
     );
